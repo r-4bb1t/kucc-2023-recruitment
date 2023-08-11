7fd57cf011f07bf754a6858f390c42ce84121753
tokki.pf <tokki.pf@kakaobrain.com>
2023-08-11 19:45:03 +0900

    meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>KUCC 2023-2 RECRUITMENT</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
